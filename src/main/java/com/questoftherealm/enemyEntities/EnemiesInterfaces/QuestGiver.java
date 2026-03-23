d14a4e14b4a65378b70d6c6543fb187bb24457a1
boza20000 <boristsvetkov912@gmail.com>
2026-03-23 17:16:19 +0200

    Remove unused files: ClientSession and QuestGiver interface

--- src/main/java/com/questoftherealm/enemyEntities/EnemiesInterfaces/QuestGiver.java
+++
@@ -1,9 +0,0 @@
-package com.questoftherealm.enemyEntities.EnemiesInterfaces;
-
-import com.questoftherealm.characters.player.Player;
-import com.questoftherealm.expeditions.quest.Quest;
-
-public interface QuestGiver {
-    Quest giveQuest(Player player);//some mobs can give you side quests
-
-}
